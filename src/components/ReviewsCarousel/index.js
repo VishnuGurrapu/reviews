10c254fb9ef1c688337d8b49bb93e8d58c81db3a
VishnuGurrapu <gurrapuvishnu20@gmail.com>
2024-08-06 15:51:38 +0000

    first commit

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,60 @@
+import {Component} from 'react'
+import './index.css'
+class ReviewsCarousel extends Component {
+  state = {
+    index: 0,
+  }
+
+  onClickNext = () => {
+    const {reviewsList} = this.props
+    const l = reviewsList.length
+    this.setState(prevState => {
+      if (prevState.index === l - 1) {
+        return {index: prevState.index}
+      }
+      return {index: prevState.index + 1}
+    })
+  }
+
+  onClickPrev = () => {
+    this.setState(prevState => {
+      if (prevState.index === 0) {
+        return {index: prevState.index}
+      }
+      return {index: prevState.index - 1}
+    })
+  }
+
+  render() {
+    const {index} = this.state
+    const {reviewsList} = this.props
+    const {imgUrl, username, companyName, description} = reviewsList[index]
+
+    return (
+      <div className="bgContainer">
+        <button className="btn" onClick={this.onClickPrev}>
+          <img
+            className="arrow"
+            alt="left arrow"
+            src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png "
+          />
+        </button>
+        <div className="reviewContainer">
+          <h1 className="username">{username}</h1>
+          <img className="profile" src={imgUrl} alt={username} />
+          <h2 className="companyname">{companyName}</h2>
+          <p className="description">{description}</p>
+        </div>
+        <button className="btn" onClick={this.onClickNext}>
+          <img
+            className="arrow"
+            alt="left arrow"
+            src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png "
+          />
+        </button>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
